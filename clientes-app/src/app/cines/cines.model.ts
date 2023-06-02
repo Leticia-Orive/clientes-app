fea48b2ed42ab3ce74a00760ddf293c5e2e8ef0d
LETICIA ORIVE PEÑA <leticia.orive.contractor@bbva.com>
2023-06-02 12:11:16 +0200

    Definir el modelo de cine

---
+++ clientes-app/src/app/cines/cines.model.ts
@@ -0,0 +1,14 @@
+export interface Cine {
+  id: number;
+  nombre: string;
+  provincia: string;
+  direccion: string;
+  codigoPostal: number;
+  telefono: number;
+  sala: number;
+}
+
+export interface Sala {
+  id: number;
+  nombre: string;
+}
